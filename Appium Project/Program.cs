f0fb608d0cd4ce8efea79d718a809361a7879f68
Mohammed Amin KOULAL <mohammedkoulal@yahoo.fr>
2019-10-26 10:03:15 +0200

    tester

--- Appium Project/Program.cs
+++ Appium Project/Program.cs
@@ -7,6 +7,8 @@ namespace Appium_Project
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
         }
     }
 }
